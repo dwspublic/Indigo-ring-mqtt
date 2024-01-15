3622b40313ee694578ec06253878cfe249ca49db
dwspublic <45836646+dwspublic@users.noreply.github.com>
2024-01-14 23:25:23 -0500

    .1

--- ring-mqtt.indigoPlugin/Contents/Server Plugin/plugin.py
+++ ring-mqtt.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -31,7 +31,7 @@ class Plugin(indigo.PluginBase):
         self.ring_devices = {}
         self.ring_battery_devices = {}
 
-        if self.pluginPrefs["ring_devices"] is None:
+        if "ring_devices" not in self.pluginPrefs:
             self.pluginPrefs["ring_devices"] = str(self.ring_devices)
 
         self.ring_devices = eval(self.pluginPrefs["ring_devices"])
